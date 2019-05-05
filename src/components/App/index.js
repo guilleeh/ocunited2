0210904a92d3af9533c43c17e7b10dbe05372da2
Elaine Ho <elaine48e@gmail.com>
2019-05-05 12:42:50 -0700

    started working on heading
    added folders for organization too

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Heading from '../Heading';
+import './index.css';
+
+class App extends React.Component{
+    render(){
+        return(
+            <Heading />
+        );
+    }
+}
+
+export default App;
