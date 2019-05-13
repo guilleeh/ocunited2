ae6f7cf396f747ca38edbe19e54eddec28474636
Elaine Ho <elaine48e@gmail.com>
2019-05-12 19:01:26 -0700

    added mission statement buttons to home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Heading from '../Heading';
 import Slideshow from './slideshow';
+import './index.css'
 
 class Home extends React.Component{
     render(){
@@ -8,6 +9,14 @@ class Home extends React.Component{
             <div>
                 <Heading />
                 <Slideshow />
+                <div className="mission-statement">
+                    <div>
+                        OC United is just that - Orange County, United.
+                    </div>
+                    <div>
+                        We’re all the people who have ever thought, “I wish I could do something.” We’re a collection of citizens, non-profits, and churches that believe we can accomplish more working together than apart.
+                    </div>
+                </div>
             </div>
         );
     }
--- src/components/Home/slideshow.js
+++ src/components/Home/slideshow.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel'
-import './image_carousel.css';
+import './slideshow.css';
 
 class Slideshow extends React.Component{
     state={
@@ -13,69 +13,96 @@ class Slideshow extends React.Component{
             require('./images/love_fullerton.jpg'),
             require('./images/love_placentia.jpg'),
             require('./images/pumpkin.jpg')
-        ]
+        ],
+
+        domestic_abuse:require('./images/da.jpg'),
+        foster_care:require('./images/fc.jpg'),
+        homelessness:require('./images/h.jpg'),
+        neighborhoods:require('./images/n.jpg')
     }
 
     render(){
         return (
-        <div className="carousel-div">
-            <Carousel
-                slide='false'
-                fade='true'
-                controls={false}
-                indicators={false}
-                pauseOnHover={false}
-            >
-                <Carousel.Item>
-                <img
-                    className="slide-item"
-                    src= {this.state.slideImages[0]}
-                    alt="OC United"/>
-                </Carousel.Item> 
-                <Carousel.Item>
-                <img
-                    className="slide-item"
-                    src= {this.state.slideImages[1]}
-                    alt="OC United"/>
-                </Carousel.Item> 
-                <Carousel.Item>
-                <img
-                    className="slide-item"
-                    src= {this.state.slideImages[2]}
-                    alt="OC United"/>
-                </Carousel.Item> 
-                <Carousel.Item>
-                <img
-                    className="slide-item"
-                    src= {this.state.slideImages[3]}
-                    alt="OC United"/>
-                </Carousel.Item> 
-                <Carousel.Item>
-                <img
-                    className="slide-item"
-                    src= {this.state.slideImages[4]}
-                    alt="OC United"/>
-                </Carousel.Item> 
-                <Carousel.Item>
-                <img
-                    className="slide-item"
-                    src= {this.state.slideImages[5]}
-                    alt="OC United"/>
-                </Carousel.Item> 
-                <Carousel.Item>
-                <img
-                    className="slide-item"
-                    src= {this.state.slideImages[6]}
-                    alt="OC United"/>
-                </Carousel.Item> 
-                <Carousel.Item>
-                <img
-                    className="slide-item"
-                    src= {this.state.slideImages[7]}
-                    alt="OC United"/>
-                </Carousel.Item> 
-            </Carousel>
-        </div>
+            <div id="black">
+                <div className="carousel-div">
+                    <Carousel
+                        slide='false'
+                        fade='true'
+                        controls={false}
+                        indicators={false}
+                        pauseOnHover={false}
+                    >
+                        <Carousel.Item>
+                        <img
+                            className="slide-item"
+                            src= {this.state.slideImages[0]}
+                            alt="OC United"
+                            draggable="false"/>
+                        </Carousel.Item> 
+                        <Carousel.Item>
+                        <img
+                            className="slide-item"
+                            src= {this.state.slideImages[1]}
+                            alt="OC United"
+                            draggable="false"
+                            />
+                        </Carousel.Item> 
+                        <Carousel.Item>
+                        <img
+                            className="slide-item"
+                            src= {this.state.slideImages[2]}
+                            alt="OC United"
+                            draggable="false"
+                            />
+                        </Carousel.Item> 
+                        <Carousel.Item>
+                        <img
+                            className="slide-item"
+                            src= {this.state.slideImages[3]}
+                            alt="OC United"
+                            draggable="false"
+                            />
+                        </Carousel.Item> 
+                        <Carousel.Item>
+                        <img
+                            className="slide-item"
+                            src= {this.state.slideImages[4]}
+                            alt="OC United"/>
+                        </Carousel.Item> 
+                        <Carousel.Item>
+                        <img
+                            className="slide-item"
+                            src= {this.state.slideImages[5]}
+                            alt="OC United"
+                            draggable="false"
+                            />
+                        </Carousel.Item> 
+                        <Carousel.Item>
+                        <img
+                            className="slide-item"
+                            src= {this.state.slideImages[6]}
+                            alt="OC United"
+                            draggable="false"
+                            />
+                        </Carousel.Item> 
+                        <Carousel.Item>
+                        <img
+                            className="slide-item"
+                            src= {this.state.slideImages[7]}
+                            alt="OC United"
+                            draggable="false"
+                            />
+                        </Carousel.Item> 
+                    </Carousel>
+                </div>
+                <div id="overlay-text">may 4th is over and they haven't updated their site sooo..</div>
+                <div className="category-container">
+                    <div className="item homelessness"> <img src={this.state.homelessness} alt="Homelessness"/> Homelessness</div>
+                    <div className="item fostercare"> <img src={this.state.foster_care} alt="Foster Care"/> Foster Care</div>
+                    <div className="item neighborhoods"> <img src={this.state.neighborhoods} alt="Neighborhoods"/> Neighborhoods</div>
+                    <div className="item domesticabuse"> <img src={this.state.domestic_abuse} alt="Domestic Abuse"/> Domestic Abuse</div>
+                </div>
+            </div>
         );
     }
 }
