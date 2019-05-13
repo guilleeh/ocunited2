5b3953de9886dec43fe827191ed148cb4454bd4b
Elaine Ho <elaine48e@gmail.com>
2019-05-13 12:16:14 -0700

    expanded home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,6 +3,8 @@ import Heading from '../Heading';
 import Slideshow from './slideshow';
 import './index.css'
 
+import Support from './images/support.jpg';
+
 class Home extends React.Component{
     render(){
         return(
@@ -10,13 +12,21 @@ class Home extends React.Component{
                 <Heading />
                 <Slideshow />
                 <div className="mission-statement">
-                    <div>
-                        OC United is just that - Orange County, United.
-                    </div>
-                    <div>
-                        We’re all the people who have ever thought, “I wish I could do something.” We’re a collection of citizens, non-profits, and churches that believe we can accomplish more working together than apart.
+                    <div className="container">
+                        <div className="child-one">
+                            <span id="orange">OC United</span> is just that - Orange County, united.
+                        </div>
+                        <div className="child-two">
+                            We’re all the people who have ever thought, “I wish I could do something.” We’re a collection of citizens, non-profits, and churches that believe we can accomplish more working together than apart.
+                        </div>
                     </div>
                 </div>
+                <div className="video-box">
+                    <img src={Support} draggable="false" alt="support"/>
+                    <h1>Will You Step Into the Gap With Us?</h1>
+                    <h2>Watch the Video Now</h2>
+                </div>
+                
             </div>
         );
     }
